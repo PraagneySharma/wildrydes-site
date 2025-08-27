d1d6fcde40d9483a3a06bcf8f7a2a5d5c33890b2
Praagney Sharma <134804351+PraagneySharma@users.noreply.github.com>
2025-08-27 16:36:19 -0400

    new client and user pool ID

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ySo5TVvlM', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '73kj3ridur9srvrh6pupspqtoc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_hIfACTywG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '291mbgn37vmrtk3rdfcr4or9lt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
